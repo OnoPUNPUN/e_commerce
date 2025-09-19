569a09099bf734f6f1184641de41eaa3df083259
OnoPUNPUN <punpunonodera754@gmail.com>
2025-09-19 11:39:11 +0600

    Made product_image_slider.dart & Updated product_details_screen.dart

--- lib/features/products/presentation/screen/product_details_screen.dart
+++ lib/features/products/presentation/screen/product_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../app/app_colors.dart';
+import '../widgets/product_image_slider.dart';
 import '../widgets/total_price_and_cart_section.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
@@ -14,15 +16,63 @@ class ProductDetailsScreen extends StatefulWidget {
 class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Product Details")),
+      appBar: AppBar(title: const Text("Product Details")),
       body: Column(
         children: [
-          Expanded(child: Column(children: [])),
-          TotalPriceAndCartSection(),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ProductImageSlider(),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nike A123 - New Edition of Jordan Sports\nSave 30%",
+                        style: textTheme.bodyLarge,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.star, size: 20, color: Colors.amber),
+                          const SizedBox(width: 4),
+                          const Text('4.2', style: TextStyle(fontSize: 16)),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Reviews',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          Card(
+                            color: AppColors.themeColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.all(2),
+                              child: Icon(
+                                Icons.favorite_outline,
+                                size: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const TotalPriceAndCartSection(),
         ],
       ),
     );
   }
 }
-
